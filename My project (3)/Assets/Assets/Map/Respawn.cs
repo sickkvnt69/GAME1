a6d4c41abb458305a8a16f15351d0f5e3cecb051
pgchmp22 <noaheh27@gmail.com>
2023-02-23 15:52:25 -0800

    respawn and simlple wrld
    not much

---
+++ My project (3)/Assets/Assets/Map/Respawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject checkpoint;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player.transform.position = checkpoint.transform.position;
+        }
+    
+    }
+}
